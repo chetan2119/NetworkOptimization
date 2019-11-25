14afa01e8158ed09cb341c86c9682bfff45a9160
chetan2119 <chetan2119@live.com>
2019-11-25 02:34:17 -0600

    Edges

---
+++ src/components/Edge.java
@@ -0,0 +1,47 @@
+package components;
+
+public class Edge {
+    private int start;
+    private int end;
+    private int weight;
+
+    public Edge(int start, int end, int weight) {
+        if (start < 0) throw new IndexOutOfBoundsException("Vertex name must be a nonnegative integer");
+        if (end < 0) throw new IndexOutOfBoundsException("Vertex name must be a nonnegative integer");
+        if (weight < 0) throw new IndexOutOfBoundsException("Weight must be a nonnegative integer");
+        
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getOtherVertex(int x) {
+        if (x == start) {
+            return end;
+        } else if (x == end) {
+            return start;
+        } else throw new IllegalArgumentException("Illegal endpoint");
+    }
+
+    @Override
+    public String toString() {
+        return start + "-" + end + " " + weight;
+    }
+
+    public static void main(String[] args) {
+        Edge e = new Edge(12, 45, 3);
+        System.out.println(e);
+    }
+}
